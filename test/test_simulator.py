be257bc7b7a82c8d80b62486b64eedd48baa0a76
Peter Cudmore <peter.cudmore@uqconnect.edu.au>
2018-04-19 16:04:43 +1000

    fixed simulator test example

--- test/test_simulator.py
+++ test/test_simulator.py
@@ -1,22 +1,31 @@
+import numpy
+
 import BondGraphTools as bgt
 from BondGraphTools.sim_tools import simulate
+
 
 def test_c_sim():
 
     c = bgt.new("C", value=0.001)
     r = bgt.new("R", value=1000)
-    Se = bgt.new("Se")
+    se = bgt.new("Se")
     kcl = bgt.new("0")
 
-    bg = c + r + Se + kcl
+    bg = c + r + se + kcl
 
     bg.connect([(c, kcl),
                 (r, kcl),
-                (Se, kcl)])
+                (se, kcl)])
 
-    [t, x] = simulate(
+    observables = se.ports[0] + c.state_vars
+
+    t, X = simulate(
         bg,
         intial=[0],
         params=["exp(-t)"],
-        observable=[Se.ports, c.state_vars]
+        observable=observables
     )
+
+    t_test = numpy.exp(-t)
+
+    assert abs(t - X[0]) < 0.0001
